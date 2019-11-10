b5fb1f11dc0287b4fbf3a01f188e5da1fed63483
aliensmart <kaoua17@gmail.com>
2019-11-10 05:16:11 -0500

    comment added on passwords result.js

--- src/components/LoggedInView/Header/Header.js
+++ src/components/LoggedInView/Header/Header.js
@@ -1,35 +1,15 @@
 import React from 'react'
 import './Header.css';
 import Search from '../Password/Search'
-import { makeStyles } from '@material-ui/core/styles';
 
 
-const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(12, 1fr)',
-      gridGap: theme.spacing(0),
-    },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-      whiteSpace: 'nowrap',
-      marginBottom: theme.spacing(1),
-    },
-    divider: {
-      margin: theme.spacing(2, 0),
-    },
-  }));
+
 
 const Header = ()=>{
-    const classes = useStyles();
-    const token = sessionStorage.getItem("token")
     return(
         <div className="Header">
             <div className="logo">Logo</div>
             <div className="profile">
-              {/* Your api key is: {token} */}
               <Search/>
             </div>
             
